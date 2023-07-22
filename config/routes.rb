5d225d3f7c2cc736338c5ad7c7bf92428ee9d467
Kate Guyer <kguyer@harding.edu>
2023-07-21 23:49:21 -0500

    Fix app to work on different computer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get "users/sign_out" => "user/sessions#destroy"
+    get "users/sign_out" => "users/sessions#destroy"
   end
   root "articles#index"
   resources :articles do
